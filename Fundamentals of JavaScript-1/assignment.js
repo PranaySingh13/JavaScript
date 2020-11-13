dc5dedcc257633c2ad6cbf701c69c7dc80ae38c4
Pranay Singh <pranay.singh.1393@gmail.com>
2020-11-13 21:40:04 +0530

    First Commited HTML File

---
+++ Fundamentals of JavaScript-1/assignment.js
@@ -0,0 +1,5 @@
+//1. Values and Variables
+let country="India";
+let continent="Asia";
+let population="1326.20 Million";
+console.log(country,continent,population);
--- Fundamentals of JavaScript-1/script.js
+++ Fundamentals of JavaScript-1/script.js
@@ -1,4 +1,31 @@
-
+//----1----
 // Linking a JavaScript File
 let js='amazing';
 console.log(10+20+30+40);
+
+//----2----
+//Values and Variables
+console.log("Jonas");
+console.log(40);
+
+//Declaring a variable
+let firstName="Matilda";
+console.log(firstName); //whenever we need to output something from our code.
+console.log(firstName);
+console.log(firstName);
+
+//Variable naming conventions
+let jonas_matilda="JM";
+let $function=27;
+let person="jonas";
+let PI=3.1415;// constant no. can be store in capitals.
+
+let myFirstJob="Coder";
+let myCurrentJob="Teacher";
+let job1="Programmer";
+let job2="Teacher";
+
+console.log(jonas_matilda,$function,person,PI,myFirstJob,myCurrentJob,job1,job2);
+
+//----3----
+//Data Types
