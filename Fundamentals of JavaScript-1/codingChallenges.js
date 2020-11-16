f7c8ffa4d6e95f3c10fd38e6f22eb4522caeea95
Pranay Singh <pranay.singh.1393@gmail.com>
2020-11-17 04:40:22 +0530

    First Coding Challenge done

---
+++ Fundamentals of JavaScript-1/codingChallenges.js
@@ -0,0 +1,14 @@
+//1. Coding Challenge
+
+const markWeight=78;
+const markHeight=1.69;
+const johnWeight=92;
+const johnHeight=1.95;
+
+const bmiMark=markWeight/markHeight**2;
+const bmiJohn=johnWeight/johnHeight**2;
+console.log(bmiMark,bmiJohn);
+
+const markHigherBMI=bmiMark>bmiJohn;
+console.log(markHigherBMI);
+
